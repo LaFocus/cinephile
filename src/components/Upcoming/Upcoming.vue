f57eca957ac511312c8f3228a38010a19534cf46
focus <chert123@gm.ru>
2023-05-26 20:16:01 +0500

    asf

---
+++ src/components/Upcoming/Upcoming.vue
@@ -0,0 +1,11 @@
+<template>
+    <transition name="upcoming" mode="out-in">
+        <div class="main__upcoming">
+            <Upcomingitem />
+        </div>
+    </transition>
+</template>
+
+<script setup>
+import Upcomingitem from '@/components/Upcoming/Upcomingitem.vue';
+</script>
