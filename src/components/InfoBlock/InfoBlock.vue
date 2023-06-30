b5acb4cfc387639b158c452f0ee53a3d4a59208e
f <f@f>
2023-06-30 21:08:44 +0500

    genresAdded

--- src/components/InfoBlock/InfoBlock.vue
+++ src/components/InfoBlock/InfoBlock.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="main__info">
         <img src="@/assets/images/close.svg" class="main__info-close" @click="$emit('close')" alt="">
-        <div class="main__info-block">
-            <img src="@/assets/images/block.jpg" class="main__info-img" alt="">
+        <div class="main__info-block" v-if="current">
+            <img :src="imgUrlFull + current.backdrop_path" class="main__info-img" alt="">
             <div class="main__info-content">
                 <div class="main__info-content-block">
-                    <h2 class="main__info-content-title">Шан-Чи и легенда десяти колец</h2>
-                    <p class="main__info-content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh
-                        bibendum nec, pulvinar id in in ac nisl semper. Condimentum tellus ac integer condimentum. Amet,
-                        vitae dictum convallis dignissim. Lacus, suscipit sagittis, adipiscing metus, risus convallis sit...
+                    <h2 class="main__info-content-title">{{ current.title || current.name}}</h2>
+                    <p class="main__info-content-text">
+                        {{ current.overview }}
                     </p>
                     <p class="main__info-content-date">
-                        <span>2023, &nbsp;</span>
-                        <span>боевик, приключения, фэнтези</span>
+                        <span>
+                        {{ 
+                            new Date (current.first_air_date).getFullYear() ||
+                            new Date (current.release_date).getFullYear()
+                        }} </span>
+                        <span>
+                            {{ getGenres }}
+                        </span>
                     </p>
+                    <BtnMore />
                 </div>
-                <BtnMore />
             </div>
         </div>
     </div>
@@ -23,6 +28,8 @@
 
 <script setup>
 import { imgUrlFull } from '@/static'
-
+import {computed} from "vue"
 const props = defineProps(['current', 'type'])
+const getGenres = computed(() => props.current.genres.reduce((acc, item) => acc + `, ${item.name}`, ""))
+
 </script>
--- src/components/MOT/Content.vue
+++ src/components/MOT/Content.vue
@@ -6,7 +6,7 @@
         </router-link>
 
         <Swiper :modules="modules" space-between="25" :navigation="true" :breakpoints="swiperOptions.breakpoints">
-            <Swiper-slide class="main__video-item" v-for="(item, idx) in content" :key="item.id">
+            <Swiper-slide class="main__video-item" v-for="(item, idx) in content" :key="item.id" @click="getItem(item)">
                 <img v-lazy="imgUrlFull + item.poster_path" src="@/assets/images/poster.png" alt=""
                     class="main__video-item-img">
                 <router-link :to="`${props.type}/`" class="main__video-item-link"/>
@@ -18,7 +18,7 @@
                 </router-link>
             </Swiper-slide>
         </Swiper>
-        <div class="main__inf" :class="{active: open}">
+        <div class="main__inf" :class="{active: open}" ref="inf">
             <InfoBlock 
                 :current="current"
                 :type="type" 
@@ -35,6 +35,7 @@ import { Navigation } from "swiper";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import { usePopular } from '@/stores/popular'
+import { useItemid } from "@/stores/itemid";
 import { onMounted, ref, computed } from "vue";
 import { imgUrl, imgUrlFull } from '@/static.js'
 import InfoBlock from "@/components/InfoBlock/InfoBlock.vue";
@@ -43,6 +44,7 @@ import InfoBlock from "@/components/InfoBlock/InfoBlock.vue";
 
 const props = defineProps(['type'])
 const popular = usePopular()
+const itemIdStore = useItemid()
 
 let modules = ref([Navigation])
 let swiperOptions = ref({
@@ -71,15 +73,26 @@ onMounted(() => {
     popular.getPopular({ type: props.type })
 })
 
+const getItem = async item => {
+    current.value = null
+    await itemIdStore.getItemId({type: props.type, id: item.id})
+    current.value = props.type == 'movie' ? itemIdStore.movie : itemIdStore.tv
 
+    open.value = true
+    let infTop = inf.value.offsetTop
+    window.scrollTo({
+        top: infTop - navHeight.offsetHeight,
+        behavior: 'smooth'
+    })
+}
 
 let current = ref(null)
 let inf = ref(null)
-
-let open = ref(true)
+let open = ref(false)
 
 const close = () => {
-    console.log(current.value);
+    open.value = false
+    current.value = null
 }
 
 </script>
